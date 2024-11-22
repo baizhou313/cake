3032978d61a77f40237221cc5d436f5342a64f6b
baizhou <1648817293@qq.com>
2024-11-22 19:54:11 +0800

    banalert

--- js/problem.js
+++ js/problem.js
@@ -1,3 +1,7 @@
+let input = document.querySelector('#input');
+const message = document.getElementById('message');
+
+
 document.addEventListener('DOMContentLoaded', () => {
 
     document.body.classList.add('fade-in');
@@ -15,18 +19,23 @@ function judge() {
     let inputValue = input.value;
     // 将输入框的值转换为数字类型
     let num = parseFloat(inputValue);
-    if (num > 1123) {
-        alert('这个数太大了,请重新输入');
+
+    message.classList.remove('success', 'error');
+    if (num > 1023) {
+        message.textContent = '这个数太大了, 请重新输入';
+        message.classList.add('error');
     }
-    else if (num < 1123) {
-        alert('这个数太小了,请重新输入');
+    else if (num < 1023) {
+        message.textContent = '这个数太小了, 请重新输入';
+        message.classList.add('error');
     }
-    else if (num == 1123) {
+    else if (num == 1023) {
         alert('恭喜你，答对了！');
         window.location.href = "../html/cake.html";
 
     }
     else {
-        alert('你这输的也不是数字呀');
+        message.textContent = '你这输的也不是数字呀';
+        message.classList.add('error');
     }
 }
